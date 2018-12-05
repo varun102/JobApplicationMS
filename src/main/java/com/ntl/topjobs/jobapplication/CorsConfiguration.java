fe716227f1edf57b6eae9889460b902cb6beb918
kvarun10@gmail.com <Varun Kashyap>
2018-12-06 04:00:52 +0530

    late night

--- src/main/java/com/ntl/topjobs/jobapplication/CorsConfiguration.java
+++ src/main/java/com/ntl/topjobs/jobapplication/CorsConfiguration.java
@@ -1,25 +1,25 @@
-package com.ntl.topjobs.jobapplication;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-//@Configuration
-//@EnableWebMvc
-public class CorsConfiguration 
-{
-//    @SuppressWarnings("deprecation")
-//	@Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("GET", "POST","DELETE");
-            }
-        };
-    }
-}
+//package com.ntl.topjobs.jobapplication;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+////@Configuration
+////@EnableWebMvc
+//public class CorsConfiguration 
+//{
+////    @SuppressWarnings("deprecation")
+////	@Bean
+//    public WebMvcConfigurer corsConfigurer()
+//    {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedMethods("GET", "POST","DELETE");
+//            }
+//        };
+//    }
+//}
